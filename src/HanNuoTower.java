da88dc98cfd8a88ae8cc9cbfee511fb09018be5a
m2lgsb <martin2lgsb@gmail.com>
2018-09-25 15:41:32 +0800

    feat: HanNuoTower

---
+++ src/HanNuoTower.java
@@ -0,0 +1,19 @@
+/**
+ * Created by m2lgsb on 2018/9/25 14:43
+ */
+public class HanNuoTower {
+    public static void main(String[] args) {
+        int nDisks = 3;
+        doTowers(nDisks, 'A', 'B', 'C');
+    }
+
+    public static void doTowers(int topN, char from, char inter, char to) {
+        if (topN == 1) {
+            System.out.println("Disk 1 from " + from + " to " + to);
+        } else {
+            doTowers(topN - 1, from, to, inter);
+            System.out.println("Disk " + topN + " from " + from + " to " + to);
+            doTowers(topN - 1, inter, from, to);
+        }
+    }
+}
